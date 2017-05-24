1af9f50b6a4495e1e14f40ad277e3d77bcab4d50
mcproger <artem7lomonosov@gmail.com>
2017-05-24 12:48:11 +0300

    Refactor database setup

--- database_setup.py
+++ database_setup.py
@@ -5,10 +5,6 @@ from dormitory_site.models import base, users
 base.Base.metadata.create_all(bind=server.engine)
 session = server.Session()
 
-vasiaUser = users.User("vasia", "Vasiliy Pypkin", "vasia2000")
-petyaUser = users.User("petya", "Petya Pypkin", "petya2000")
-session.add(vasiaUser)
-session.add(petyaUser)
-session.commit()
+
 
 print('DB created!')
