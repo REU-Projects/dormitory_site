29a9a07a3af59d1d71f0422cbb360f3abe16f8d3
mcproger <artem7lomonosov@gmail.com>
2017-07-15 20:10:24 +0300

    Fix db settings

--- dormitory_site/server.py
+++ dormitory_site/server.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import SQLALCHEMY_DATABASE_URI
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
 
 from dormitory_site import views
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-Session = sessionmaker(bind=engine)
