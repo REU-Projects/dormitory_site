f9fe63b65896514155dd2cbebd581c7743419030
mcproger <artem7lomonosov@gmail.com>
2017-07-16 12:40:00 +0300

    Fix run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 #!flask/bin/python
 from dormitory_site.server import app
 
-app.run(debug = True)
+port = int(os.environ.get('PORT', 5000))
+
+if port == 5000:
+    app.debug = True
+
+app.run(host='0.0.0.0', port=port)
