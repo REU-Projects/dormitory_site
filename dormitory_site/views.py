31136f4157e413451cbc4dbb80b0e19b175e2a0c
mcproger <artem7lomonosov@gmail.com>
2017-07-16 12:09:53 +0300

    Some fixes

--- dormitory_site/views.py
+++ dormitory_site/views.py
@@ -1,33 +1,15 @@
 from flask import render_template, flash, redirect
 from dormitory_site.server import app
 from dormitory_site.forms.login_form import LoginForm
+from dormitory_site.models.users import User
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
-    posts = [ # список выдуманных постов
-        { 
-            'author': { 'nickname': 'John' }, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': { 'nickname': 'Susan' }, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-    return render_template("index.html",
-        title = 'Home',
-        user = user,
-        posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login in as: "' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(fullname=form.openid.data).first_or_404()
+        return render_template('show_user.html', user=user)
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
