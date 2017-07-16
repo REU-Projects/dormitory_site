9ac00fe6e9ad67fd393b2bdbb66a452c1227a316
mcproger <artem7lomonosov@gmail.com>
2017-07-16 12:08:29 +0300

    Add some data

--- database_setup.py
+++ database_setup.py
@@ -2,4 +2,9 @@ from dormitory_site.server import db
 from dormitory_site.models import users 
 db.create_all()
 
+vasya = users.User("Zufra", "FMESI", "1", "417")
+
+db.session.add(vasya)
+db.session.commit()
+
 print("DB created!")
