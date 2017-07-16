5c930857897d72e737dc04d3b254e94508709ce7
mcproger <artem7lomonosov@gmail.com>
2017-07-16 13:43:32 +0300

    Some fixes

--- dormitory_site/views.py
+++ dormitory_site/views.py
@@ -4,7 +4,7 @@ from dormitory_site.forms.login_form import LoginForm
 from dormitory_site.models.users import User
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
